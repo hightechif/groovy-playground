7a8fa690462df607923eada44e005e3e28d14322
R. Fadhil <ridhanfadhilah@gmail.com>
2021-05-27 16:46:40 +0700

    conditional operators

---
+++ gr07_conditional/Main.groovy
@@ -0,0 +1,9 @@
+class Main {
+    static void main(String[] args) {
+
+        // 7. Conditional Operators
+        assert (!true) == false
+        assert (!'foo') == false
+        assert (!'') == true
+    }
+}
