34b875e684a50fa8b2639257fb2376355275eacd
R. Fadhil <ridhanfadhilah@gmail.com>
2021-05-27 16:45:34 +0700

    relational operators

---
+++ gr04_relational/Main.groovy
@@ -0,0 +1,13 @@
+class Main {
+    static void main(String[] args) {
+
+        // 4. Relational operators
+        assert 1 + 2 == 3
+        assert 3 != 4
+        assert -2 < 3
+        assert 2 <= 3
+        assert 3 <= 4
+        assert 5 > 1
+        assert 5 >= -2
+    }
+}
