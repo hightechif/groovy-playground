3a143c1deb53c43f2df1dd2d045f0a26cbefd99f
R. Fadhil <ridhanfadhilah@gmail.com>
2021-05-27 18:36:28 +0700

    conditional operators

--- gr07_conditional/Main.groovy
+++ gr07_conditional/Main.groovy
@@ -2,8 +2,19 @@ class Main {
     static void main(String[] args) {
 
         // 7. Conditional Operators
+        // 7.1. Not operator
         assert (!true) == false
         assert (!'foo') == false
         assert (!'') == true
+
+        // 7.2. Ternary operator
+        def x = (byte) 8
+        def isEven = (x % 2 == 0) ? "x is even" : "x is odd"
+        println(isEven)
+
+        // 7.3. Elvis operator
+        def username
+        def displayName = username ?: 'Anonymous'
+        println(displayName)
     }
 }
